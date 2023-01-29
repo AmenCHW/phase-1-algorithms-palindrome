5a1afc5dafb47377ebcb2d9815e2837d8a229769
Ameen Abdulsamad <amenchawshly98@gmail.com>
2023-01-29 15:58:53 +0300

    commit tree 3bafe425f486464264555546154ac5dc0206f3ab

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const arrayWord = word.split('')
+  const reverseArrayWord = arrayWord.reverse()
+  const reverseWord = reverseArrayWord.join('')
+
+  if (word === reverseWord){
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+  turn the string into an array
+  reverse the array values 
+  join the array
+  compare the strig to the reverse value
 */
 
 /*
-  Add written explanation of your solution here
+  we turned the word into an array then reversed the array and ,erged it, then compared itt to the original word, if the words 
+  were equal that means they are a palindrome
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("labal", isPalindrome("racecar"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("alex", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
